3a2ee6c1198de0ab8322942b5b24c68551c8b22b
Bumbi <annika.borgesson@gmail.com>
2023-11-08 21:20:37 +0100

    Add func. to loop until  valid intupo data -> email,age

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+"""
+Import needed for the code
+"""
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -11,21 +14,26 @@ SCOPE = [
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.OPEN('financial-freedom-calc')
+SHEET = GSPREAD_CLIENT.open('financial-freedom-calc')
 
 
 def get_user_data():
     """
-    Collect user data like name, email and address.
+    Collect user data like name, email, and address.
     """
     name = input('Enter your name: ')
     while True:
-        try:
-            email = input('Enter your email: ')
-            if "@" in email and "." in email and email.find("@") < email.rfind('.'):
-                break
-        except:
+        email = input('Enter your email: ')
+        if "@" in email and "." in email and email.find("@") < email.rfind('.'):
+            break
+        else:
             print('Invalid email address, please try again!')
 
+    while True:
+        age = input('Enter your age here: ')
+        if age.isdigit():
+            break
+        else:
+            print('Invalid data: age must be in digits, please try again!')
+
 get_user_data()
-    
