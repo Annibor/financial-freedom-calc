ed8fc8567b884d6329f6c5a1d31fd8b784a4ea67
Bumbi <annika.borgesson@gmail.com>
2023-11-14 19:53:29 +0100

    Add func. enter name &  welcome message

--- run.py
+++ run.py
@@ -15,3 +15,16 @@ CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('financial-freedom-calc')
+
+def user_data():
+    """
+    Get user data and welcome to the program.
+    """
+    print('Welcome to the financial freedom calculator!\n')
+    name = input('Please enter your name: \n')
+    print(f'Hello {name}!\n')
+    print('This program will calculate the number of years it takes'
+          'to reach finanicial freedom, or how much you need to save'
+          'each month to reach the finanicial freedom in a certain years.\n')
+
+    return name
