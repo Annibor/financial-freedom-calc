483533f540c991b52675d1970671cea73b8769f3
Bumbi <annika.borgesson@gmail.com>
2023-11-14 19:57:53 +0100

    Add base for choise of calculation

--- run.py
+++ run.py
@@ -27,4 +27,19 @@ def user_data():
           'to reach finanicial freedom, or how much you need to save'
           'each month to reach the finanicial freedom in a certain years.\n')
 
-print(user_data())
+def choose_what_to_calc():
+    """
+    Choose what to calculate.
+    """
+    print('Please choose what you want to calculate:\n')
+    print('1. How many years it takes to reach the finanicial freedom\n')
+    print('2. How much you need to save each month to reach the finanicial'
+          'freedom in a certain years\n')
+    choice = input('Enter your choice: \n')
+    if choice == '1':
+        how_many_years()
+    elif choice == '2':
+        how_much_to_save_each_month()
+    else:
+        print('Invalid choice. Please try again.\n')
+        choose_what_to_calc()
