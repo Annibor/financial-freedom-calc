e8dfbaf8c20011e223d7c608ba10023380703048
Bumbi <annika.borgesson@gmail.com>
2023-11-18 19:49:51 +0100

    Add euro to calc. inputs

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('financial-freedom-calc')
 
+
 def user_data():
     """
     Get user data and welcome to the program.
@@ -39,18 +40,19 @@ def choose_what_to_calc():
     print('2. How much you need to save each month to reach the finanicial'
           'freedom in a certain years\n')
     choice = input('Enter your choice: \n')
+    
     if choice == '1':
-        initial_savings = float(input('Please enter your initial savings: \n'))
-        monthly_savings = float(input('Please enter your monthly savings: \n'))
-        financial_goal = float(input('Please enter your financial goal: \n'))
+        initial_savings = float(input('Please enter your initial savings in euro: \n'))
+        monthly_savings = float(input('Please enter your monthly savings in euro: \n'))
+        financial_goal = float(input('Please enter your financial goal in euro: \n'))
         annual_interest = float(input('Please enter your annual interest rate: \n'))
         monthly_savings_percent = float(input('Please enter your monthly savings percentage: \n'))
         years_to_financial_freedom = CalcYearsToFinancialFreedom(initial_savings, monthly_savings, financial_goal, annual_interest, monthly_savings_percent)
 
         return years_to_financial_freedom
     elif choice == '2':
-        initial_savings_two = float(input('Please enter your initial savings: \n'))
-        target_goal_two = float(input('Please enter your target goal: \n'))
+        initial_savings_two = float(input('Please enter your initial savings in euro: \n'))
+        target_goal_two = float(input('Please enter your target goal in euro: \n'))
         taget_years_to_freedom = float(input('Please enter your taget years to freedom: \n'))
         required_monthly_savings = CalcRequiredMonthlySavings(initial_savings_two, target_goal_two, taget_years_to_freedom)
 
@@ -58,7 +60,6 @@ def choose_what_to_calc():
     else:
         print('Invalid choice. Please try again.\n')
         choose_what_to_calc()
-
 
 user_name = user_data()
 calculation_choice = choose_what_to_calc()
@@ -70,3 +71,5 @@ if isinstance(calculation_choice, CalcYearsToFinancialFreedom):
 elif isinstance(calculation_choice, CalcRequiredMonthlySavings):
     result = calculation_choice.calc_required_monthly_savings()
     print(f'{user_name}, you will need to save {result:.2f} euros every month to reach your financial goal.\n')
+
+
