87cb68e2fac445d56c692f9ce8dd31cc2f606af9
Bumbi <annika.borgesson@gmail.com>
2023-11-20 11:52:17 +0100

    Delete double try, except code from calculations.py

--- calculations.py
+++ calculations.py
@@ -17,12 +17,9 @@ class CalcYearsToFinancialFreedom:
     Calculates the number of years it takes to reach the finanicial freedom.
     """
     def __init__(self, initial_savings, monthly_savings, financial_goal):
-        try:
-            self.initial_savings = float(initial_savings)
-            self.monthly_savings = float(monthly_savings)
-            self.financial_goal = float(financial_goal)
-        except ValueError as exc:
-            raise ValueError('Invalid input. Answers must be numeric values. Please try again.\n') from exc
+        self.initial_savings = float(initial_savings)
+        self.monthly_savings = float(monthly_savings)
+        self.financial_goal = float(financial_goal)
 
     def calc_years_to_financial_freedom(self):
         """
@@ -42,12 +39,10 @@ class CalcRequiredMonthlySavings:
     Calculates the required monthly savings to reazch a specific financial target.
     """
     def __init__(self, initial_savings_two, target_goal_two, target_years_to_freedom):
-        try:
-            self.initial_savings_two = float(initial_savings_two)
-            self.target_goal_two = float(target_goal_two)
-            self.target_years_to_freedom = float(target_years_to_freedom)
-        except ValueError as exc:
-            raise ValueError('Invalid input. Answers must be numeric values. Please try again.\n') from exc
+        self.initial_savings_two = float(initial_savings_two)
+        self.target_goal_two = float(target_goal_two)
+        self.target_years_to_freedom = float(target_years_to_freedom)
+
 
     def calc_required_monthly_savings(self):
         """
