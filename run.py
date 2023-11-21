032bf69e4e7b1ac7ffdad043a32b02b641c6bd80
Bumbi <annika.borgesson@gmail.com>
2023-11-21 14:59:57 +0100

    Add some comments for code. start break the code in more lines

--- run.py
+++ run.py
@@ -9,12 +9,16 @@ from calculations import CalcRequiredMonthlySavings
 def user_data():
     """
     Get user data and welcome to the program.
+
+    Returns:
+         str: User's name.
     """
     print('Welcome to the financial freedom calculator!\n')
     name = input('Please enter your name: \n')
     print(f'Hello {name}!\n')
     check_if_exit(name)
 
+    # This gives user a introduction to the calculator.
     print('This program will calculate the number of years it takes'
           'to reach finanicial freedom, or how much you need to save'
           'each month to reach the finanicial freedom in a certain years.\n')
@@ -24,7 +28,11 @@ def user_data():
 def choose_what_to_calc():
     """
     Choose what to calculate.
+
+    Returns:
+        Object: Instance of either CalcYearsTiFinancialFreedom or CalcRequiredMothlySavings.
     """
+    # This gives the 
     print('Please choose what you want to calculate:\n')
     print('1. How many years it takes to reach the finanicial freedom\n')
     print('2. How much you need to save each month to reach the finanicial'
@@ -34,16 +42,22 @@ def choose_what_to_calc():
 
     if choice == '1':
         try:
-            initial_savings = float(input('Please enter your initial savings in euro: \n'))
-            monthly_savings = float(input('Please enter your monthly savings in euro: \n'))
-            financial_goal = float(input('Please enter your financial goal in euro: \n'))
-            years_to_financial_freedom = CalcYearsToFinancialFreedom(initial_savings, monthly_savings, financial_goal)
+            # Get user inputs for the first calcualtion.
+            initial_savings = (float(input(
+                'Please enter your initial savings in euro: \n')))
+            monthly_savings = (float(input(
+                'Please enter your monthly savings in euro: \n')))
+            financial_goal = (float(input(
+                'Please enter your financial goal in euro: \n')))
+            years_to_financial_freedom = (CalcYearsToFinancialFreedom(
+                initial_savings, monthly_savings, financial_goal))
             return years_to_financial_freedom
         except ValueError:
             print('Invalid input. Answers must be numeric values. Please try again.\n')
             return choose_what_to_calc()
     elif choice == '2':
         try:
+            # Get user inputs for the second calculation. 
             initial_savings_two = float(input('Please enter your initial savings in euro: \n'))
             target_goal_two = float(input('Please enter your target goal in euro: \n'))
             taget_years_to_freedom = float(input('Please enter your taget years to freedom: \n'))
@@ -67,15 +81,18 @@ def run_calc():
         calculation_choice = choose_what_to_calc()
 
         if isinstance(calculation_choice, CalcYearsToFinancialFreedom):
+            # This will show the results of the first calculation for the user, and the name the user added in the beginning of the program will be shown.
             result = calculation_choice.calc_years_to_financial_freedom()
             print(f'{user_name}, it will take {result} years '
                   'to reach the finanicial freedom.\n')
 
         elif isinstance(calculation_choice, CalcRequiredMonthlySavings):
+            # This will show the results of the second calculation for the user, and the name the user added in the beginning of the program will be shown.
             result = calculation_choice.calc_required_monthly_savings()
             print(f'{user_name}, you will need to save {result:.2f} euros '
                   'every month to reach your financial goal.\n')
 
+        # This will give the user a choice if user wants to make another calculation or not. 
         repeat = input('Do you want to make a new calculation? (yes/no): ').lower()
         check_if_exit(repeat)
 
