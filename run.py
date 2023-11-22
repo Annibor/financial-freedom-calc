5199576d446444823cb909c41f85573f45d5c20c
Bumbi <annika.borgesson@gmail.com>
2023-11-22 11:20:58 +0100

    Connected googlesheet into run.py

--- run.py
+++ run.py
@@ -4,9 +4,22 @@
 """
 Import needed for the code
 """
+import gspread
+from google.oauth2.service_account import Credentials
 from calculations import check_if_exit
 from calculations import CalcYearsToFinancialFreedom
 from calculations import CalcRequiredMonthlySavings
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('financial-freedom-calc')
 
 
 def user_data():
