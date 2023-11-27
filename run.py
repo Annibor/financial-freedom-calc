0a0d41e2b10a52ede50230017a39b16eecc344f7
Bumbi <annika.borgesson@gmail.com>
2023-11-27 09:10:39 +0100

    run.py fix whitespace & line lenght

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-#This code is based on two different sources:
+# This code is based on two different sources:
 # one by ismailmoufid47, available at GitHu(link in README.md )
 # and one by Akash3121, available at GitHub (link in README.md).
 """
@@ -21,6 +21,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('financial-freedom-calc')
 
+
 def update_user_worksheet(user_data_list):
     """Update the user worksheet. Add new
     information to the worksheet from user input.
@@ -29,6 +30,7 @@ def update_user_worksheet(user_data_list):
     user_worksheet = SHEET.worksheet('user_sheet')
     user_worksheet.append_row(user_data_list)
     print('User worksheet updated')
+
 
 def update_financial_worksheet_one(financial_data_list_one):
     """
@@ -39,6 +41,7 @@ def update_financial_worksheet_one(financial_data_list_one):
     financial_worksheet = SHEET.worksheet('financial_sheet_one')
     financial_worksheet.append_row(financial_data_list_one)
     print('Financial worksheet updated')
+
 
 def update_financial_worksheet_two(financial_data_list_two):
     """
@@ -106,7 +109,8 @@ freedom in a certain years\n
                 'Please enter your financial goal in euro: \n')))
             years_to_financial_freedom = (CalcYearsToFinancialFreedom(
                 initial_savings, monthly_savings, financial_goal))
-            financial_data_list_one = [initial_savings, monthly_savings, financial_goal]
+            financial_data_list_one = [initial_savings, monthly_savings,
+                                       financial_goal]
             update_financial_worksheet_one(financial_data_list_one)
 
             return years_to_financial_freedom
@@ -126,7 +130,8 @@ freedom in a certain years\n
                 'Please enter your taget years to freedom: \n')))
             required_monthly_savings = (CalcRequiredMonthlySavings(
                 initial_savings_two, target_goal_two, target_years_to_freedom))
-            financial_data_list_two = [initial_savings_two,target_goal_two, target_years_to_freedom]
+            financial_data_list_two = [initial_savings_two, target_goal_two,
+                                       target_years_to_freedom]
             update_financial_worksheet_two(financial_data_list_two)
             return required_monthly_savings
         except ValueError:
