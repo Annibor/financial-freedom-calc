82fad24056f1387a0ad7a288cb4f8df89f3f5f48
Bumbi <annika.borgesson@gmail.com>
2023-11-27 09:02:59 +0100

    Fix whitespace & line lenght

--- calculations.py
+++ calculations.py
@@ -2,6 +2,7 @@
 Calculations for the Financial Freedom Calculator.
 """
 
+
 def check_if_exit(input_value):
     """
     Check if the input value is 'exit' and exit the calculator if true.
@@ -28,27 +29,31 @@ class CalcYearsToFinancialFreedom:
         if self.monthly_savings >= self.financial_goal:
             return 0
 
-        years_to_target = (self.financial_goal - self.initial_savings) / (self.monthly_savings * 12)
+        years_to_target = ((self.financial_goal - self.initial_savings)
+                           / (self.monthly_savings * 12))
 
         return years_to_target
 
 
 class CalcRequiredMonthlySavings:
     """
-    Calculates the required monthly savings to reazch a specific financial target.
+    Calculates the required monthly savings to
+    reach a specific financial target.
     """
-    def __init__(self, initial_savings_two, target_goal_two, target_years_to_freedom):
+    def __init__(self, initial_savings_two, target_goal_two,
+                 target_years_to_freedom):
         self.initial_savings_two = float(initial_savings_two)
         self.target_goal_two = float(target_goal_two)
         self.target_years_to_freedom = float(target_years_to_freedom)
 
-
     def calc_required_monthly_savings(self):
         """
-        Calculates the required monthly savings to reazch a specifiv financial target.
+        Calculates the required monthly savings to reazch
+        a specifiv financial target.
         """
         periods = self.target_years_to_freedom * 12
-        monthly_savings_required = (self.target_goal_two - self.initial_savings_two) / periods
-
+        monthly_savings_required = ((self.target_goal_two -
+                                     self.initial_savings_two)
+                                    / periods)
 
         return monthly_savings_required
